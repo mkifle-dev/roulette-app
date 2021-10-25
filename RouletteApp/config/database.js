63150eebe52dd3356b4b903d4c455d00aa1575f7
Miriam Kifle <88691480+mkifle-dev@users.noreply.github.com>
2021-10-25 11:51:41 -0400

    Update database.js

--- RouletteApp/config/database.js
+++ RouletteApp/config/database.js
@@ -1,6 +1,10 @@
 // config/database.js
 module.exports = {
 
-    'url' : '', 
-    'dbName': ''
+    'url' : 'mongodb+srv://auth-1:auth@cluster0.tdpfk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', 
+    'auth-1': 'auth'
+
+
+    
 };
+//const url = "mongodb+srv://user-one:BlueMoon@cluster0.5xllp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
